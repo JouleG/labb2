20bb367982a07af351f219b7cb3154aa965b0e37
markuSkold <markus.skold@chasacademy.se>
2021-09-29 13:27:47 +0200

    new

--- markus.py
+++ markus.py
@@ -7,15 +7,15 @@ def cone(radie_cone, height_cone):
 #Function that calculates the volume of the cone.   
 
 
-form = input("\n\nHello! Tell me the form of the figure. \nPlease enter 1 if it´s a sphere or 2 if it´s a cone")
+form = input("\n\nWelcome! Tell me the form of the figure. \n\nPlease enter 1 if it´s a sphere or 2 if it´s a cone?\n")
 #create variabel so i can use it in functions. Need to be input about the form. 
 
 if form =="1":
-    radie_sphere=float(input("fdfdf please enter the radie of yout sphere"))
-    print(f"Great, if the radie of your sphere is {radie_sphere} cm then the volume is", sphere(radie_sphere), "cm^3.")
+    radie_sphere=float(input("\nPlease enter the radie of the sphere!\n"))
+    print(f"\nIf the radie of your sphere is {radie_sphere} cm then the volume is", sphere(radie_sphere), "cm^3.")
 elif form =="2":
-    radie_cone=float(input("fdfdf please enter the radie of yout cone"))
-    height_cone=float(input("fdfdf please enter the height of yout cone"))
-    print(f"Great, if the radie of your cone is {radie_cone} and the height is {height_cone} cm then the volume is", cone(radie_cone, height_cone), "cm^3.")
+    radie_cone=float(input("\nPlease enter the radie of the cone!\n"))
+    height_cone=float(input("\nPlease enter the height of the cone!\n"))
+    print(f"\nIf the radie of the cone is {radie_cone} and the height is {height_cone} cm then the volume is", cone(radie_cone, height_cone), "cm^3.")
 else: 
     print("Sorry, can´t understand. Please try again.")
